6a699fdee02e4cea2c85608ce410d3703927f77f
Mart Aarma <mart.aarma@nortal.com>
2023-11-21 11:20:55 +0200

    MDL-8 Add empty Spring Boot application

---
+++ src/main/java/ee/ria/eudi/qeaa/rp/RpApplication.java
@@ -0,0 +1,13 @@
+package ee.ria.eudi.qeaa.rp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ee/ria/eudi/qeaa/rp/RpApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.ria.eudi.qeaa.rp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
