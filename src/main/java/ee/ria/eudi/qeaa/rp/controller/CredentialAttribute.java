d24e99b50008c3c4049f91369fd7ca906d9db0e7
Mart Aarma <mart.aarma@nortal.com>
2025-01-14 10:34:39 +0200

    Add ARF 1.4 PID rulebook conformance

--- src/main/java/ee/ria/eudi/qeaa/rp/controller/CredentialAttribute.java
+++ src/main/java/ee/ria/eudi/qeaa/rp/controller/CredentialAttribute.java
@@ -9,15 +9,19 @@ import java.util.List;
 @Getter
 @RequiredArgsConstructor
 public enum CredentialAttribute {
+    EU_EUROPA_EC_EUDI_PID_EE_1_PERSONAL_ADMINISTRATIVE_NUMBER(CredentialDoctype.EU_EUROPA_EC_EUDI_PID_1, CredentialNamespace.EU_EUROPA_EC_EUDI_PID_1, "personal_administrative_number"),
     EU_EUROPA_EC_EUDI_PID_1_FAMILY_NAME(CredentialDoctype.EU_EUROPA_EC_EUDI_PID_1, CredentialNamespace.EU_EUROPA_EC_EUDI_PID_1, "family_name"),
     EU_EUROPA_EC_EUDI_PID_1_GIVEN_NAME(CredentialDoctype.EU_EUROPA_EC_EUDI_PID_1, CredentialNamespace.EU_EUROPA_EC_EUDI_PID_1, "given_name"),
+    EU_EUROPA_EC_EUDI_PID_1_GIVEN_NAME_BIRTH(CredentialDoctype.EU_EUROPA_EC_EUDI_PID_1, CredentialNamespace.EU_EUROPA_EC_EUDI_PID_1, "given_name_birth"),
+    EU_EUROPA_EC_EUDI_PID_1_FAMILY_NAME_BIRTH(CredentialDoctype.EU_EUROPA_EC_EUDI_PID_1, CredentialNamespace.EU_EUROPA_EC_EUDI_PID_1, "family_name_birth"),
+    EU_EUROPA_EC_EUDI_PID_1_BIRTH_PLACE(CredentialDoctype.EU_EUROPA_EC_EUDI_PID_1, CredentialNamespace.EU_EUROPA_EC_EUDI_PID_1, "birth_place"),
     EU_EUROPA_EC_EUDI_PID_1_BIRTH_DATE(CredentialDoctype.EU_EUROPA_EC_EUDI_PID_1, CredentialNamespace.EU_EUROPA_EC_EUDI_PID_1, "birth_date"),
     EU_EUROPA_EC_EUDI_PID_1_ISSUANCE_DATE(CredentialDoctype.EU_EUROPA_EC_EUDI_PID_1, CredentialNamespace.EU_EUROPA_EC_EUDI_PID_1, "issuance_date"),
     EU_EUROPA_EC_EUDI_PID_1_EXPIRY_DATE(CredentialDoctype.EU_EUROPA_EC_EUDI_PID_1, CredentialNamespace.EU_EUROPA_EC_EUDI_PID_1, "expiry_date"),
     EU_EUROPA_EC_EUDI_PID_1_ISSUING_AUTHORITY(CredentialDoctype.EU_EUROPA_EC_EUDI_PID_1, CredentialNamespace.EU_EUROPA_EC_EUDI_PID_1, "issuing_authority"),
     EU_EUROPA_EC_EUDI_PID_1_ISSUING_COUNTRY(CredentialDoctype.EU_EUROPA_EC_EUDI_PID_1, CredentialNamespace.EU_EUROPA_EC_EUDI_PID_1, "issuing_country"),
-    EU_EUROPA_EC_EUDI_PID_1_DOCUMENT_NUMBER(CredentialDoctype.EU_EUROPA_EC_EUDI_PID_1, CredentialNamespace.EU_EUROPA_EC_EUDI_PID_1, "document_number"),
-    EU_EUROPA_EC_EUDI_PID_EE_1_PERSONAL_IDENTIFICATION_NUMBER(CredentialDoctype.EU_EUROPA_EC_EUDI_PID_1, CredentialNamespace.EU_EUROPA_EC_EUDI_PID_EE_1, "personal_identification_number"),
+    EU_EUROPA_EC_EUDI_PID_1_NATIONALITY(CredentialDoctype.EU_EUROPA_EC_EUDI_PID_1, CredentialNamespace.EU_EUROPA_EC_EUDI_PID_1, "nationality"),
+    U_EUROPA_EC_EUDI_PID_1_AGE_OVER_18(CredentialDoctype.EU_EUROPA_EC_EUDI_PID_1, CredentialNamespace.EU_EUROPA_EC_EUDI_PID_1, "age_over_18"),
 
     ORG_ISO_18013_5_1_FAMILY_NAME(CredentialDoctype.ORG_ISO_18013_5_1_MDL, CredentialNamespace.ORG_ISO_18013_5_1, "family_name"),
     ORG_ISO_18013_5_1_GIVEN_NAME(CredentialDoctype.ORG_ISO_18013_5_1_MDL, CredentialNamespace.ORG_ISO_18013_5_1, "given_name"),
